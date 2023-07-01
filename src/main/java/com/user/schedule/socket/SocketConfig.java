8914f7d89b5817d548ffbf11cdacbc39be9cb627
Mahdi <test@gmail.com>
2023-07-01 06:42:40 +0330

    set origin

--- src/main/java/com/user/schedule/socket/SocketConfig.java
+++ src/main/java/com/user/schedule/socket/SocketConfig.java
@@ -4,8 +4,10 @@ import com.corundumstudio.socketio.SocketIOServer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins = "*")
 public class SocketConfig {
 
     @Value("${socket-server.host}")
@@ -19,6 +21,7 @@ public class SocketConfig {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(host);
         config.setPort(port);
+        config.setOrigin("*");
         return new SocketIOServer(config);
     }
 
